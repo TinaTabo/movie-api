52b110d23f7116f05034ec176fa616aa6c70944a
Cristina Taboada <cris.taboada98@gmail.com>
2023-07-23 09:57:20 +0200

    inicialización del proyecto apiRest

---
+++ src/test/kotlin/tinatabo/kotlinApiRest/movieapi/MovieApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package tinatabo.kotlinApiRest.movieapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MovieApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
