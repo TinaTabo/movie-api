7c9ba9f871ee176c3a21ac2ad510e3795eb9aa3b
Cristina Taboada <cris.taboada98@gmail.com>
2023-07-23 10:14:51 +0200

    GET Hello World!

--- src/main/kotlin/tinatabo/kotlinApiRest/movieapi/MovieApiApplication.kt
+++ src/main/kotlin/tinatabo/kotlinApiRest/movieapi/MovieApiApplication.kt
@@ -2,9 +2,16 @@ package tinatabo.kotlinApiRest.movieapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
+
+@RestController
 @SpringBootApplication
-class MovieApiApplication
+class MovieApiApplication{
+	@GetMapping("/hello")
+	fun helloWorld() = "Hello World!"
+}
 
 fun main(args: Array<String>) {
 	runApplication<MovieApiApplication>(*args)
